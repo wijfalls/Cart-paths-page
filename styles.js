ef982539c20a6d7be3523ad4cb554065f9025d46
William Falls <wfallmc@gmail.com>
2025-09-18 23:31:35 +0900

    Create styles.js

---
+++ styles.js
@@ -0,0 +1,24 @@
+centerContent: {
+  flex: 1,
+  justifyContent: 'center',
+  alignItems: 'center',
+  paddingHorizontal: 20,
+},
+
+comingSoonImage: {
+  width: 120,
+  height: 120,
+  marginBottom: 20,
+},
+
+comingSoonTitle: {
+  fontSize: 24,
+  color: Theme.black,
+  marginBottom: 10,
+},
+
+comingSoonSubtitle: {
+  fontSize: 16,
+  color: Theme.placeholder,
+  textAlign: 'center',
+},
